d8e9e23c3bc24e0b892dd60fc715a6fe3fc1fd16
Landon Volkmann <lsvr34@mail.umkc.edu>
2018-11-13 13:40:04 -0600

    Function Stubs

--- Folder.cpp
+++ Folder.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "Folder.h"
+
+void Folder::add_folder(string path, string folder_name)
+{
+}
+
+void Folder::delete_folder(string path, string folder_name)
+{
+}
+
+void Folder::add_file(string path, string file_name, int size)
+{
+}
+
+File Folder::get_file(string path, string file_name)
+{
+	return File();
+}
+
+list<File> Folder::get_files(string path, string file_name)
+{
+	return list<File>();
+}
+
+void Folder::delete_file(string path, string file_name)
+{
+}
--- Folder.h
+++ Folder.h
@@ -5,11 +5,32 @@
 #ifndef FILETREE_FOLDER_H
 #define FILETREE_FOLDER_H
 
+#include<string>
+#include<list>
+using namespace std;
+
+struct File {
+	string name;
+	int size;
+};
 
 class Folder {
+
 private:
-    //List<*Folder> childFolders;
+
+	string name;
+	int size; //in bytes
+    list<Folder> childFolders;
     //RBTree<File> childFiles;
+
+public:
+
+	void add_folder(string path, string folder_name);
+	void delete_folder(string path, string folder_name);
+	void add_file(string path, string file_name, int size);
+	File get_file(string path, string file_name);
+	list<File> get_files(string path, string file_name);
+	void delete_file(string path, string file_name);
 
 };
 
